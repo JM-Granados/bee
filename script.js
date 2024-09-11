9a9600dccedf7a8b076682ea7ca206f418ce0a15
Jose <99422768+JM-Granados@users.noreply.github.com>
2024-09-11 03:12:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const gif = document.querySelector(".gif");
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Por supuesto que si, eres la mujer más guapa e inteligente que conozco, me encantas boba...";
-  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+  gif.src = "https://media.giphy.com/media/zrxazUScjhxo4/giphy.gif?cid=ecf05e47df38p3x03rlxj9jf4csmk204v7y4ukdgas7uhlh9&ep=v1_gifs_search&rid=giphy.gif&ct=g";
 });
 
 // Make the No button move randomly on hover
